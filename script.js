15b7d776af55ab3111d6ccfed333cf6ee2642610
Mamabolo Khutjo <162458416+MamaboloKhutjo@users.noreply.github.com>
2024-10-28 16:24:35 +0200

    worked on the drop down navigation for the mobile

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const navToggle = document.querySelector('.nav-toggle');
 const navHeader = document.querySelector('.nav-header');
 
 navToggle.addEventListener('click', () => {
-    navHeader.classList.toggle('active'); // Toggle the active class
+    navHeader.classList.toggle('active'); 
 });
