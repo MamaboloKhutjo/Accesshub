e5ff6342913120a84b810ad893bf0ca9c4939767
Mamabolo Khutjo <162458416+MamaboloKhutjo@users.noreply.github.com>
2024-10-29 18:07:47 +0200

    ...

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ const signInButton=document.getElementById('signInButton');
 const signUpForm = document.getElementById('signUp');
 const signInForm = document.getElementById('signIn');
 
+signInButton.addEventListener('click', function(){
+    signInForm.style.display= "block";
+    signUpForm.style.display= "none"; 
+})
+
 signUpButton.addEventListener('click', function(){
     signInForm.style.display= "none";
     signUpForm.style.display= "block";
 })
 
-signInButton.addEventListener('click', function(){
-    signInForm.style.display= "block";
-    signUpForm.style.display= "none"; 
-})
+
 
 const navToggle = document.querySelector('.nav-toggle');
 const navHeader = document.querySelector('.nav-header');
