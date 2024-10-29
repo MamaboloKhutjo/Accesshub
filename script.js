0836bb1c99c90071c2327aff2bc2787a1c3c184d
Mamabolo Khutjo <162458416+MamaboloKhutjo@users.noreply.github.com>
2024-10-29 18:02:11 +0200

    got the forms to change interchanbly depending of if the user wants to sign in or sign up

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const signUpButton=document.getElementById('SignUpButton');
+const signInButton=document.getElementById('signInButton');
+const signUpForm = document.getElementById('signUp');
+const signInForm = document.getElementById('signIn');
+
+signUpButton.addEventListener('click', function(){
+    signInForm.style.display= "none";
+    signUpForm.style.display= "block";
+})
+
+signInButton.addEventListener('click', function(){
+    signInForm.style.display= "block";
+    signUpForm.style.display= "none"; 
+})
+
 const navToggle = document.querySelector('.nav-toggle');
 const navHeader = document.querySelector('.nav-header');
 
